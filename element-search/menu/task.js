0cb4caf1a150fdeee92b8cae6231f7d0d78f653c
bombik815 <58844959+bombik815@users.noreply.github.com>
2022-03-14 17:40:58 +0300

    second commit
    Поменял логику меню на клик мышки.

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,19 @@
-const items = document.getElementsByClassName('menu__item');
+const  menuItems = document.querySelectorAll('.menu__item');
+const menuLink = document.querySelectorAll('.menu__link');
 
 
+menuLink.forEach(e => {
+	e.addEventListener('click', function (e) {
+		let target = e.target;
+		let targetParent = target.closest('.menu__item');
 
-for(let i=0; i< items.length; i++){
-	items[i].addEventListener('mouseenter', showSub, false);
-	items[i].addEventListener('mouseleave', hideSub, false);
-}
-
-function showSub(){
-	if(this.children.length>1){
-		this.children[1].style.display = 'block';
-	}else{
-		return false;
-	}
-}
-
-function hideSub(){
-	if(this.children.length>1){
-		this.children[1].style.display = 'none';
-	}else{
-		return false;
-	}
-}
+		if (targetParent) {
+			let subm = targetParent.getElementsByClassName('menu_sub')[0];
+			if (!subm.classList.contains('menu_active')) {
+			  subm.classList.add('menu_active');
+			}else{
+				subm.classList.remove('menu_active');
+			}
+		 }
+	});
+});
