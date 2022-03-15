5f2227f4d015a76005369318ebdbc339d77a6280
bombik815 <58844959+bombik815@users.noreply.github.com>
2022-03-15 12:20:15 +0300

    second commit
    Поменял логику и добавил точки индикатор  на слайдер.

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,20 +1,40 @@
 const next = document.querySelector('.slider__arrow_next'),
 		prev = document.querySelector('.slider__arrow_prev');
-const slides = document.querySelectorAll('.slider__item');
+const slides = document.getElementsByClassName("slider__item");
+const dots = document.getElementsByClassName("slider__dot");
 
-let currentSlide = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-next.onclick = () => {
-	goToSlide(currentSlide+1);
+next.addEventListener('click', function (){
+	showSlides(slideIndex += 1);
+});
+
+prev.addEventListener('click', function (){
+	showSlides(slideIndex -= 1); 
+});
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
 }
 
-prev.onclick = () => {
-	goToSlide(currentSlide-1);
+function showSlides(n) {
+	let i;
+
+	if (n > slides.length) {
+	  slideIndex = 1;
 	}
+	if (n < 1) {
+		 slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		 slides[i].classList.remove('slider__item_active');
+	}
+	for (i = 0; i < dots.length; i++) {
+		 dots[i].className = dots[i].className.replace(" slider__dot_active", "");
+	}
+	slides[slideIndex-1].classList.add('slider__item_active');
+	dots[slideIndex - 1].className += " slider__dot_active";
+}
 
 
-function goToSlide(n) {
-	slides[currentSlide].className = 'slider__item';
-	currentSlide = (currentSlide+1)%slides.length;
-	slides[currentSlide].className = 'slider__item slider__item_active';
-  }
