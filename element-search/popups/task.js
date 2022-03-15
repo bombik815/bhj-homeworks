10824b8b0574759ff19d382ced5a9b2e0750a7c8
bombik815 <58844959+bombik815@users.noreply.github.com>
2022-03-15 10:04:07 +0300

    second commit
    Изменил логику срипта.

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,29 +1,34 @@
-const modal = document.querySelector('.modal');
+
+const modal = document.querySelector('#modal_main');
+const modalSuccess = document.querySelector('#modal_success');
+
 const modelActive = document.getElementsByClassName('.modal_active');
 const closeButton = modal.getElementsByClassName('modal__close')[0];
-const btn_red = document.querySelector('.btn');
-const greenBtn = document.querySelector("#modal_success");
-const redBtn = document.querySelector("#modal_main");
+const btnRed = document.querySelector('.btn');
 
 
-document.addEventListener('click', function(e) {
-	modal.classList.add('modal_active');
-});
+// window.onclick =  function(e) {
+// 	modal.classList.add('modal_active');
+// };
+var delayPopup = 3000;
+setTimeout("document.getElementById('modal_main').classList.add('modal_active')", delayPopup);
 
 // закрываем модальное окно на кнопку X
-closeButton.onclick = function(e) {
+modal.getElementsByClassName('modal__close')[0].onclick = function(e) {
 	e.stopPropagation();
 	modal.classList.remove('modal_active');
  };
 
+// закрываем модальное окно на кнопку X
+modalSuccess.getElementsByClassName('modal__close')[0].onclick = function(e) {
+	e.stopPropagation();
+	modalSuccess.classList.remove('modal_active');
+ };
 
- btn_red.onclick = function(e)  {
-	 if (btn_red.classList.contains('btn_success')){
-		btn_red.classList.toggle('btn_success');
-		btn_red.textContent = 'Сделать хорошо';
-	 }else {
-		btn_red.classList.toggle('btn_success');
-		btn_red.classList.toggle('show-success');
-		btn_red.textContent = 'Хорошо сделано!';
-	 }
+
+ btnRed.onclick = function(e)  {
+	 if (modal.classList.contains('modal_active')){
+		modal.classList.remove('modal_active');
+		modalSuccess.classList.add('modal_active');
+	}
 };
